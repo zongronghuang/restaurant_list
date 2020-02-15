a07251caa147740d1c9c711450e22a15f55779ff
zongronghuang <zongrong.h886@gmail.com>
2020-02-15 18:19:31 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+// 引入 express 和 handlebars 和資料 json
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+// 使用 main.handlebars 作為基本模版
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}))
+
+// 指定 handlebars 作為渲染引擎
+app.set('view engine', 'handlebars')
+
+// 指定靜態資料夾
+app.use(express.static('public'))
+
+// 設定路由
+app.get('/', (req, res) => {
+  res.render('index', {
+    restaurants: restaurantList.results
+  })
+})
+
+
+// 設定 server 監聽器
+app.listen(port, (req, res) => {
+  console.log(`Server running. 
+Server URL: http://localhost:${port}`)
+})
