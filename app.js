0d4c79c5cee156810c1f2902beeca87b82f7616a
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-02-17 00:00:22 +0800

    Add search and detail pages

--- app.js
+++ app.js
@@ -17,16 +17,36 @@ app.set('view engine', 'handlebars')
 // 指定靜態資料夾
 app.use(express.static('public'))
 
-// 設定路由
+// 設定首頁路由
 app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+// 設定搜尋路由
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurantMatches = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()))
+
   res.render('index', {
-    restaurants: restaurantList.results
+    restaurants: restaurantMatches,
+    keyword: req.query.keyword
   })
 })
+
+// 設定餐廳詳細資料路由
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  id = req.params.restaurant_id
+  restaurantById = restaurantList.results.find(restaurant => id === restaurant.id.toString())
+
+  res.render('show', { restaurant: restaurantById })
+
+
+})
+
 
 
 // 設定 server 監聽器
 app.listen(port, (req, res) => {
   console.log(`Server running. 
-Server URL: http://localhost:${port}`)
+    Server URL: http://localhost:${port}`)
 })
